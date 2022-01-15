72b0e885aff85491cf2a41cc7026326b1d682a7e
senthilnathan <libertariansenthil@gmail.com>
2022-01-15 10:46:25 +0530

    ALS.py -updated

--- Src/ALS.py
+++ Src/ALS.py
@@ -1,16 +1,23 @@
-import Filter as Ft
+import sys,os
+
 import absentees as ab
-import sys
-import os
+import Filter as Ft
+import output 
 
 __version__ = "2022.1.1"
 
-doc_path= 'D:\\Python\\programs\\Projects\\Attendance-Project\\Data\\'
+# Paths to access the Temp and Data directories. 
+doc_path= os.getcwd()+'\\Data\\help.txt'
+RM_dupe_path= os.getcwd()+'\\Temp\\rm_dupe.temp.txt'
+NameList_Path = os.getcwd()+"\\Data\\NameList.txt"
+absentees_path = os.getcwd()+'\\Temp\\absentees.temp.txt'
 
 def workingdirectory(path):
+    """
+    Convert a given path into actual workable path inside the ALS(Absentees-List-Software).
+    """
     directory = path.split("\\")
     new_directory = ""
-    
     for i in range(len(directory)-1):
         new_directory += directory[i]+'\\'
     new_directory +=directory[-1]
@@ -18,20 +25,36 @@ def workingdirectory(path):
     return new_directory
 
 def document(obj):
+    """
+    Prin the content in the 'obj'(Document) in the terminal
+    """
     if obj == "help":
-        with open(doc_path+'help.txt','r') as h:
+        with open(doc_path,'r') as h:
             doc = h.read()
         for i in doc:
             print(i,end="")
 
 if __name__ =="__main__":
-    ar1 = sys.argv[1]
+    print("\nAbsentees-List-Software:\nVersion:",__version__)   # Print the version of ALS.
+    ar1 = input("Options: ")
 
-    if ar1 in ["-v","--version"]:
-        print(__version__)
-    elif ar1 in ['-h','--help']:
+    if ar1 in ['-h','--help']:
         document('help')
     elif ar1 in ["-a", "--attendance"]:
         src= input("Paste the entire textfile location: ")
-        Ft.st_attended(workingdirectory(src))
-        ab.student_absent()
+        
+        Ft.st_attended(workingdirectory(src))       # Stores the Students present in the session.
+        ab.student_absent(Source1=NameList_Path,source2=RM_dupe_path,destination=absentees_path)    # Stores the Students(AIDS Dept.) absent in the session.
+
+        sub = input("Enter the subject name: ")
+        session = input("Enter the timings of the session: ")
+        output.op_terminal(subject= sub, session= session, strength= 58)    # Prints the absentees details in the terminal
+    elif ar1 in ['-q','--quit']:
+        pass
+    elif ar1 in ['-as','--allsource']:
+        print("\nThis feature is currently under dvelopment")
+    else:
+        print("\nSomething went wrong plz try again!!")
+
+    input("\n\npress any key to exit... ")
+    
